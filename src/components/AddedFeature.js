5579f71ad477c26c1ad54c88bc345fb8f966c344
Michael Walter <Michael_Patrick.Walter@outlook.com>
2020-02-18 15:31:54 -0800

    App, index, actions, completed

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { removeFeature } from '../actions/PrimaryActions';
 
 const AddedFeature = props => {
+
+console.log(props)
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button className="button" onClick={onClickFeature}>X</button>
       {props.feature.name}
     </li>
   );
 };
 
-export default AddedFeature;
+const mapStateToProps = (state) => {
+  return { props: state }
+};
+
+
+export default connect(
+  mapStateToProps,
+  { removeFeature }
+)(AddedFeature);
